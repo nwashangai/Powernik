8e294e7668c21aa5f2549542e8b0c61336a599f3
nwashangai <nwashangai@gmail.com>
2020-02-03 17:53:33 +0100

    chore(server-setup): setup server configurations and linting
      - setup typescript for dev
      - setup testing environment
      - setup scss for styling
      - setup css and ts lint
      - setup pre-commit hooks
      - setup material UI for UI look and feel
      - setup redux store
    
    [Maintains: server-setup]

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+const App = () => {
+  return (
+    <div className="home-page">
+      <div className="home-page__welcome">Welcome to Powernik</div>
+    </div>
+  );
+};
+
+export default App;
